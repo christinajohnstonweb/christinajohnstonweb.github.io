cd8654350762b98f6cb28e2ff2ab59d6b6414a17
jonlaplante <jonlaplante@hotmail.com>
2017-11-22 15:23:59 -0500

    Pushing video responsiveness to production.

--- web/javascripts/main.js
+++ web/javascripts/main.js
@@ -1,71 +1,3 @@
-function responsiveTest(){
-  // Resize info blocks, based on width.
-  $(".section-img").parent().parent().css( "height", parseInt( $(".content-section").width() )/2 );
-  $(".section-img").parent().prev().css( "height", parseInt( $(".content-section").width() )/2 );
-  
-//   $(".section-img").parent().parent().css( "height", $(".section-img").width() );
-//   $(".section-img").parent().prev().css( "height", $(".section-img").width() );
-
-  if (window.matchMedia("(min-width: 1201px)").matches) {
-  /* the viewport is at most 1200 pixels wide */
-
-  }
-
-  if (window.matchMedia("(min-width: 769px)").matches || window.matchMedia("(device-min-width: 769px)").matches) {
-  /* the viewport is at most 768 pixels wide */
-    $(".section-img").parent().removeClass("col-12");
-    $(".section-img").parent().prev().removeClass("col-12");
-
-    $(".section-img").parent().addClass("col-6");
-    $(".section-img").parent().prev().addClass("col-6");
-
-    $(".section-img").addClass("img-fluid");
-
-    // Handle the footer.
-    $(document).find(".footer-break").remove();
-
-    $(".footer-newsletter-container").children().first().addClass("pull-right");
-    $(".footer-newsletter-container").addClass("col-md-4");
-    $(".footer-newsletter-container").removeClass("col-11");
-   }
-
-
-  // If the viewport < 1200px, make the article sections as tall as they are wide.
-  if (window.matchMedia("(max-width: 1200px)").matches) {
-  /* the viewport is at least 1200 pixels wide */     
-  }
-
-
-  // If the viewport < 850px, make the footer contact info stacked instead of horizontal.
-  if( window.matchMedia("(max-width: 850px)".matches) ){
-//     console.log("Smaller than 850px.");
-  }
-
-
-  if (window.matchMedia("(max-width: 768px)").matches || window.matchMedia("(device-max-width: 768px)").matches) {
-  /* the viewport is at least 768 pixels wide */
-    $(".section-img").parent().addClass("col-12");
-    $(".section-img").parent().prev().addClass("col-12");
-
-    $(".section-img").parent().removeClass("col-6");
-    $(".section-img").parent().prev().removeClass("col-6");
-
-    // We're wrapping the cols, so get rid of the row height.
-    $(".section-img").parent().parent().css( "height", "" );
-
-    if( $(".footer-break") ) {
-      // Handle the footer, too.
-      $(".footer-newsletter-container").before('<div class="w-100 footer-break"></div>');
-      $(".footer-newsletter-container").children().first().removeClass("pull-right");
-      $(".footer-newsletter-container").removeClass("col-md-4");
-      $(".footer-newsletter-container").addClass("col-11");
-
-      // Handle layout of the text section.
-      $(".footer-col-no-title").before('<div class="w-100 footer-break"></div>');
-    }
-  }
-}
-
 // Parse querystrings.
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -87,7 +19,7 @@ $(function(){
     // Resize info blocks, based on width.
     setTimeout(function(){
       responsiveTest();
-    }, 3000);
+    }, 1500);
 
     
     setTimeout(function(){
---
+++ web/javascripts/media_queries.js
@@ -0,0 +1,118 @@
+function responsiveTest(){
+  // Resize info blocks, based on width.
+  $(".section-img").parent().parent().css( "height", parseInt( $(".content-section").width() )/2 );
+  $(".section-img").parent().prev().css( "height", parseInt( $(".content-section").width() )/2 );
+  
+//   $(".section-img").parent().parent().css( "height", $(".section-img").width() );
+//   $(".section-img").parent().prev().css( "height", $(".section-img").width() );
+
+  if (window.matchMedia("(min-width: 769px)").matches || window.matchMedia("(device-min-width: 769px)").matches) {
+  /* the viewport is at least 768 pixels wide */
+    $(".section-img").parent().removeClass("col-12");
+    $(".section-img").parent().prev().removeClass("col-12");
+
+    $(".section-img").parent().addClass("col-6");
+    $(".section-img").parent().prev().addClass("col-6");
+
+    $(".section-img").addClass("img-fluid");
+
+    // Handle the footer.
+    $(document).find(".footer-break").remove();
+
+    $(".footer-newsletter-container").children().first().addClass("pull-right");
+    $(".footer-newsletter-container").addClass("col-md-4");
+    $(".footer-newsletter-container").removeClass("col-11");
+    
+    // Handle video sizes.
+    $("section.video > iframe").css("width", "375px");
+    $("section.video > iframe").css("height", "211px");
+   }
+  
+  /* the viewport is 768 pixels or less wide */
+  if (window.matchMedia("(max-width: 768px)").matches || window.matchMedia("(device-max-width: 768px)").matches) {
+  /* the viewport is at least 768 pixels wide */
+    $(".section-img").parent().addClass("col-12");
+    $(".section-img").parent().prev().addClass("col-12");
+
+    $(".section-img").parent().removeClass("col-6");
+    $(".section-img").parent().prev().removeClass("col-6");
+
+    // We're wrapping the cols, so get rid of the row height.
+    $(".section-img").parent().parent().css( "height", "" );
+
+    if( $(".footer-break") ) {
+      // Handle the footer, too.
+      $(".footer-newsletter-container").before('<div class="w-100 footer-break"></div>');
+      $(".footer-newsletter-container").children().first().removeClass("pull-right");
+      $(".footer-newsletter-container").removeClass("col-md-4");
+      $(".footer-newsletter-container").addClass("col-11");
+
+      // Handle layout of the text section.
+      $(".footer-col-no-title").before('<div class="w-100 footer-break"></div>');
+      
+      // Handle video sizes.
+      $("section.video > iframe").css("width", "300px");
+      $("section.video > iframe").css("height", "169px");
+    }
+  }
+  
+  // If the viewport < 850px, make the footer contact info stacked instead of horizontal.
+  if( window.matchMedia("(max-width: 850px)".matches) ){
+  //     console.log("Smaller than 850px.");
+  }
+  
+  if (window.matchMedia("(min-width: 1201px)").matches || window.matchMedia("(device-min-width: 1201px)").matches) {
+  /* the viewport is at most 1200 pixels wide */
+  // Handle video sizes.
+    
+    $("section.video > iframe").css("width", "400px");
+    $("section.video > iframe").css("height", "225px");
+  }
+
+  // If the viewport < 1200px, make the article sections as tall as they are wide.
+  if (window.matchMedia("(max-width: 1200px)").matches) {
+  /* the viewport is at least 1200 pixels wide */     
+  }
+
+  if (window.matchMedia("(min-width: 1381px)").matches || window.matchMedia("(device-min-width: 1381px)").matches) {
+  /* the viewport is at most 1200 pixels wide */
+  // Handle video sizes.
+    $("section.video > iframe").css("width", "425px");
+    $("section.video > iframe").css("height", "239px");
+  }
+  
+  if (window.matchMedia("(min-width: 1551px)").matches || window.matchMedia("(device-min-width: 1551px)").matches) {
+  /* the viewport is at most 1200 pixels wide */
+  // Handle video sizes.
+    $("section.video > iframe").css("width", "450px");
+    $("section.video > iframe").css("height", "253px");
+  }
+  
+  // >1600px.
+  if (window.matchMedia("(min-width: 1660px)").matches || window.matchMedia("(device-min-width: 1660px)").matches) {
+    // Handle video sizes.
+    $("section.video > iframe").css("width", "475px");
+    $("section.video > iframe").css("height", "267px");
+  }
+  
+  // >1750px.
+  if (window.matchMedia("(min-width: 1750px)").matches || window.matchMedia("(device-min-width: 1750px)").matches) {
+    // Handle video sizes.
+    $("section.video > iframe").css("width", "500px");
+    $("section.video > iframe").css("height", "281px");
+  }
+
+    // >2050px.
+  if (window.matchMedia("(min-width: 2050px)").matches || window.matchMedia("(device-min-width: 2050px)").matches) {
+    // Handle video sizes.
+    $("section.video > iframe").css("width", "575px");
+    $("section.video > iframe").css("height", "324px");
+  }
+  
+     // >2300px.
+  if (window.matchMedia("(min-width: 2300px)").matches || window.matchMedia("(device-min-width: 2300px)").matches) {
+    // Handle video sizes.
+    $("section.video > iframe").css("width", "650px");
+    $("section.video > iframe").css("height", "366px");
+  }
+}
